df430584ac45ac971af68081bf189dc99d7dc322
Gianluca Starke <gianlucastarke@gmail.com>
2023-05-31 15:25:36 -0300

    app usuario está imprimindo template corretamente

--- gerenciamento_bibliotecas/gerenciamento_bibliotecas/settings.py
+++ gerenciamento_bibliotecas/gerenciamento_bibliotecas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuario.apps.UsuarioConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,10 @@ ROOT_URLCONF = 'gerenciamento_bibliotecas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'usuario/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
