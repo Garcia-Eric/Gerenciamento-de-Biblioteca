eb37d1aad4671d069e2188d85bca878d3a74611b
Gianluca Starke <gianlucastarke@gmail.com>
2023-06-02 17:14:08 -0300

    continuada tela home

--- gerenciamento_bibliotecas/home/views.py
+++ gerenciamento_bibliotecas/home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        
+        'usuarios': [
+            {
+                'nome': 'Gianluca',
+                'livro': 'O Poder do Agora',
+                'dias_vencidos': 19,
+            }
+        ]
     }
 
-    return render(request, 'home/home_adm.html')
+    return render(request, 'home/home.html', context)
