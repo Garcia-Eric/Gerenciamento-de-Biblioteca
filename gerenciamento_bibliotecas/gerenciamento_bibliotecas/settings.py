8372306c75c424f86257b068028db39af20c8743
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-06-13 16:03:09 -0300

    correções na conexão do banco

--- gerenciamento_bibliotecas/gerenciamento_bibliotecas/settings.py
+++ gerenciamento_bibliotecas/gerenciamento_bibliotecas/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'gerenciamento_bibliotecas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gerenciamento_biblioteca',
+        'USER': 'user',
+        'PASSWORD': 'user_password',
     }
 }
 
